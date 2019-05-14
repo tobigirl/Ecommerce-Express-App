6166db4fe3a96400b5466786ffbda127ce02fd18
tobigirl <tobby0696@gmail.com>
2019-05-14 14:31:22 +0100

    Add model folder

--- model/login.js
+++ model/login.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 let LoginSchema = new Schema({
 	"username": String,
-	"password": String
+	"password": String	
 })
 
 module.exports = mongoose.model('Login', LoginSchema)
